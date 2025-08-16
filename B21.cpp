34694b525bbd9903da5479ac8ed4fa356b0351dd
ilove265 <taanluis653@gmail.com>
2025-08-17 02:19:22 +0700

    Add files via upload

---
+++ B21.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int n; cin>>n;
+	int x,k;
+	cin>>x>>k;
+	int a[n + 1];
+	for ( int i = 0; i < n+1; i++){
+		if ( i == k-1 ) {
+			a[i] = x;
+			continue;	
+		}
+		cin>>a[i];
+	}
+	for ( int i = 0; i < n+1; i++){
+		cout<<a[i]<<" ";
+	}
+	return 0;
+}
