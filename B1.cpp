a0cc21494773446693c5ea8dcbf069d77a3903a5
ilove265 <taanluis653@gmail.com>
2025-08-17 02:32:44 +0700

    Add files via upload

---
+++ B1.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+string str;
+bool check = true;
+void sinh(){
+	int i = str.size()-1;
+	cout<<i<<endl;
+	while (i > 0 && str[i] == '1'){
+		str[i] = '0';
+		i--;
+	}
+	if ( i < 0) check = false;
+	else str[i] =  '1';
+}
+int main(){
+	cin>>str;		
+	sinh();
+	for ( int i = 0; i < str.size(); i++){
+		cout<<str[i];
+	}
+
+	return 0;
+}
